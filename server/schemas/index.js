5b8482f62527a1a356c50a1e2a3b4aa485c075d7
Punkinut <heypunkinut@gmail.com>
2021-07-29 10:22:38 +1000

    Added basic code to index.js file in schemas folder

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,1 +1,5 @@
-// Comment
+// Exporting the two files to be used in ther server.js
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
