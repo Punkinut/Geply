ee5633d22be64b283545dc32c35f50c8b05db76c
Punkinut <heypunkinut@gmail.com>
2021-07-30 11:32:09 +1000

    Added login and signup pages with react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LetsGo from './pages/LetsGo'
+import SignUp from './pages/SignUp'
 import './styles/reset.css'
+import './styles/style.css'
 
 function App() {
     return (
-        <div>
-            <p>Hello</p>
-        </div>
+        <Router>
+            <>
+            <Switch>
+                <Route exact path='/login' component={LetsGo} />
+                <Route exact path='/signup' component={SignUp} />
+            </Switch>   
+            </>
+        </Router>
+        
     )
 }
 
