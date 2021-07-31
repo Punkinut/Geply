dddcc53397ac61e55d9e6fcf265461ae672bfa62
Punkinut <heypunkinut@gmail.com>
2021-07-31 10:58:27 +1000

    Added handleInputChange function

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
 import Button from '../components/Tools/Button'
 
 function Login() {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({...formState, [name]: value });
+      };
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
@@ -18,8 +24,8 @@ function Login() {
         <section className='login-container'>
             <p className='header'>Login</p>
             <form className='signup-form' onSubmit={handleFormSubmit}>
-                <input type='email' placeholder='Email' className='input' required></input>
-                <input minLength='8' type='password' placeholder='Password' className='input'></input>
+                <input name='email' onChange={handleChange} value={formState.email} type='email' placeholder='Email' className='input' required></input>
+                <input name='password' onChange={handleChange} minLength='8' type='password' placeholder='Password' className='input'></input>
                 <Button type='submit' word="Login"/>
             </form>
             <Link className='lil-gray' to={'/signup'}><motion.p whileHover={{opacity: 1, scale: 1.1}}>Sign Up?</motion.p></Link>
