91a652cf9f7b319c935867e1476600d75b24ee78
Punkinut <heypunkinut@gmail.com>
2021-07-31 13:00:44 +1000

    Added auth to the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import  { Redirect } from 'react-router-dom'
+import Auth from '../utils/auth';
 
 function Home() {
+    if (!Auth.loggedIn()){
+    return <Redirect to='/welcome'/>
+    }
     return (
         <>
             <p>Home</p>
