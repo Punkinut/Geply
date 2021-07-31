95580113f78382fce190cb1ec1bbbbc0930e2255
Punkinut <heypunkinut@gmail.com>
2021-07-31 11:04:43 +1000

    Added apollo server to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { AnimatePresence } from 'framer-motion'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Fade from 'react-reveal/Fade';
 import LetsGo from './pages/LetsGo'
 import SignUp from './pages/SignUp'
@@ -8,23 +10,43 @@ import Login from './pages/Login';
 import './styles/reset.css'
 import './styles/style.css'
 
+const httpLink = createHttpLink({
+    uri: '/graphql',
+  });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
 function App() {
     return (
-        <Router>
-            <>
-            <Fade>
-                <AnimatePresence>
-                    <Switch>
-                        <Route exact path='/welcome' component={LetsGo} />
-                        <Route exact path='/login' component={Login} />
-                        <Route exact path='/signup' component={SignUp} />
-                    </Switch> 
-                </AnimatePresence>
-            
-            </Fade>
-            </>
-        </Router>
-        
+        <ApolloProvider client={client}>
+            <Router>
+                <>
+                <Fade>
+                    <AnimatePresence>
+                        <Switch>
+                            <Route exact path='/welcome' component={LetsGo} />
+                            <Route exact path='/login' component={Login} />
+                            <Route exact path='/signup' component={SignUp} />
+                        </Switch> 
+                    </AnimatePresence>
+                
+                </Fade>
+                </>
+            </Router>
+        </ApolloProvider>
     )
 }
 
