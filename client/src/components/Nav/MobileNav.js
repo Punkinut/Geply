6db0bb1021366757fe77d7242c9765ad84ec8a11
Punkinut <heypunkinut@gmail.com>
2021-08-01 13:40:35 +1000

    Nav link now changes color depending on route

--- client/src/components/Nav/MobileNav.js
+++ client/src/components/Nav/MobileNav.js
@@ -3,16 +3,10 @@ import Auth from '../../utils/auth';
 import Message from '../../images/Message.svg'
 import Compass from '../../images/Compass.svg'
 import User from '../../images/User.svg'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { motion } from 'framer-motion';
 
 function MobileNav() {
-
-    const changeColor = () => {
-        console.log('Hello')
-    }
-   
-
     if (Auth.loggedIn()) {
         return (
             <motion.div
@@ -22,9 +16,9 @@ function MobileNav() {
             exit={{ opacity: 0}}
             >
                 <section className='mobile-nav'>
-                    <Link to={'/friends'}><img className='icon' alt='Compass Icon' src={Compass}/></Link>
-                    <Link to={'/'}><img className='icon' alt='Message Icon' src={Message} onClick={changeColor}/></Link>
-                    <Link to={'/profile'}><img className='icon user' alt='User Icon' src={User}/></Link>
+                    <NavLink exact activeClassName='active-icon' className={'icon'} to='/friends'><img alt='Compass Icon' src={Compass}/></NavLink>
+                    <NavLink exact activeClassName='active-icon' className={'icon'} to='/'><img alt='Message Icon' src={Message}/></NavLink>
+                    <NavLink exact activeClassName='active-icon' className={'icon user'} to='/profile'><img alt='User Icon' src={User}/></NavLink>
                 </section>
             </motion.div>
         )
