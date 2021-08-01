c0f12cf13baa244ebeac9ad098f6bce3dbfffadc
Punkinut <heypunkinut@gmail.com>
2021-08-01 16:58:39 +1000

    Added logo to desktop navigation

--- client/src/components/Nav/DesktopNav.js
+++ client/src/components/Nav/DesktopNav.js
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import React from 'react'
+import Logo from '../../images/Logo.svg'
 import Message from '../../images/Message.svg'
 import Compass from '../../images/Compass.svg'
 import User from '../../images/User.svg'
@@ -16,6 +17,7 @@ function DesktopNav() {
             exit={{ opacity: 0}}
             >
             <section className='desktop-nav'>
+                    <NavLink  className={'icon active-icon desktop-icon logo'} to='/friends'><motion.img whileHover={{scale: 1.1}} alt='Logo Icon' src={Logo}/></NavLink>
                     <NavLink exact activeClassName='active-icon' className={'icon desktop-icon'} to='/friends'><motion.img whileHover={{scale: 1.3, rotate: '90deg'}} alt='Compass Icon' src={Compass}/></NavLink>
                     <NavLink exact activeClassName='active-icon' className={'icon desktop-icon'} to='/'><motion.img whileHover={{scale: 1.3, borderRadius: '10px'}} alt='Message Icon' src={Message}/></NavLink>
                     <NavLink exact activeClassName='active-icon' className={'icon user desktop-icon user-d'} to='/profile'><motion.img whileHover={{scale: 1.3}} alt='User Icon' src={User}/></NavLink>
