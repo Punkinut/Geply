753ab660a986172e9c8f1f0ed1cd433374786771
Punkinut <heypunkinut@gmail.com>
2021-08-01 12:01:36 +1000

    Added basic mobile nav

--- client/src/components/Nav/MobileNav.js
+++ client/src/components/Nav/MobileNav.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Auth from '../../utils/auth';
+import Message from '../../images/Message.svg'
+import Compass from '../../images/Compass.svg'
+import User from '../../images/User.svg'
+import { motion } from 'framer-motion';
 
 function MobileNav() {
     if (Auth.loggedIn()) {
         return (
-            <>
-                <p>NAV</p>
-            </>
+            <motion.div
+            initial={{ opacity: 0}}
+            animate={{ opacity: 1}}
+            transition={{ delay: 2 }}
+            exit={{ opacity: 0}}
+            >
+                <section className='mobile-nav'>
+                    <img className='icon' alt='Compass Icon' src={Compass}/>
+                    <img className='icon' alt='Message Icon' src={Message}/>
+                    <img className='icon user' alt='User Icon' src={User}/>
+                </section>
+            </motion.div>
         )
     }
     return <></>
