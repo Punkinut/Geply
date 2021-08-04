d580abd2e5ff18b859e24d5fe529ac240cbd2846
Punkinut <heypunkinut@gmail.com>
2021-08-04 10:28:33 +1000

    Styled bio input page

--- client/src/pages/Bio.js
+++ client/src/pages/Bio.js
@@ -1,21 +1,30 @@
 import { motion } from 'framer-motion'
 import React from 'react'
-import { Redirect } from 'react-router-dom';
+import Arrow from '../images/left-arrow.svg'
+import { Link, Redirect } from 'react-router-dom';
 import Auth from '../utils/auth';
 
 function Bio() {
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        console.log('Works')
+    };
     if (!Auth.loggedIn()){
         return <Redirect to='/welcome'/>
         }
     return (
-        <motion.div
-        initial={{ opacity: 0}}
-        animate={{ opacity: 1}}
-        exit={{ opacity: 0}}
-        >
+        <motion.div>
             <section className='page-container'>
                 <section className='edit-container'>
-                    <p>Bio</p>
+                    <Link to='/profile'><img className='icon left-arrow' src={Arrow} alt='Arrow Icon'/></Link>
+                    <motion.p initial={{ opacity: 0}} animate={{ opacity: 1}} className='username account-header'>Bio</motion.p>
+                    <motion.form onSubmit={handleFormSubmit} initial={{ opacity: 0}} animate={{ opacity: 1}} className='bio-form'>
+                        <textarea placeholder='Write about you...' maxLength='150' type='text' className='bio-input light-text'></textarea>
+                        <motion.button whileHover={{scale: 0.95}} type='submit' className='logout'>
+                            <p>Save</p>
+                        </motion.button>
+                    </motion.form>
                 </section>
             </section>
             
