884d6205b3a495351902ec0aa51ae8d14d98ed57
Punkinut <heypunkinut@gmail.com>
2021-08-06 16:27:25 +1000

    Add post button now hides on the add post page

--- client/src/components/Nav/AddPost.js
+++ client/src/components/Nav/AddPost.js
@@ -1,31 +1,32 @@
 import React from 'react'
 import Auth from '../../utils/auth';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { motion } from 'framer-motion';
 import Add from '../../images/add.svg'
 
 function AddPost() {
-    if (Auth.loggedIn()) {
-        return (
-            <motion.div
-            initial={{ opacity: 0}}
-            animate={{ opacity: 1}}
-            transition={{ delay: 2 }}
-            exit={{ opacity: 0}}
-            >   
-               
-               <Link to='/upload' className='addPost'>
-                    <img className='plus-icon' alt='Add Icon' src={Add}/>
-                </Link>
-               <Link to='/upload'>
-                   <motion.div className='addPostDes' whileHover={{scale: 1.1, rotate: '90deg', borderRadius: '50px'}}>
-                    <img className='plus-icon' alt='Add Icon' src={Add}/>
-                   </motion.div>
-                </Link>
-            </motion.div>
-        )
+    const location = useLocation();
+    if (!Auth.loggedIn() || location.pathname ==='/upload') {
+        return <></>
     }
-    return <></>
+    return (
+        <motion.div
+        initial={{ opacity: 0}}
+        animate={{ opacity: 1}}
+        transition={{ delay: 1 }}
+        exit={{ opacity: 0}}
+        >   
+           
+           <Link to='/upload' className='addPost'>
+                <img className='plus-icon' alt='Add Icon' src={Add}/>
+            </Link>
+           <Link to='/upload'>
+               <motion.div className='addPostDes' whileHover={{scale: 1.1, rotate: '90deg', borderRadius: '50px'}}>
+                <img className='plus-icon' alt='Add Icon' src={Add}/>
+               </motion.div>
+            </Link>
+        </motion.div>
+    )
 }
 
 export default AddPost
