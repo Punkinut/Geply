3f05f760a0d3bd19f0489bf32b3790d5e57543ef
Punkinut <heypunkinut@gmail.com>
2021-08-06 10:29:53 +1000

    Added conditional display to following page if you have none

--- client/src/pages/Following.js
+++ client/src/pages/Following.js
@@ -4,13 +4,16 @@ import React from 'react'
 import  { Link, Redirect } from 'react-router-dom'
 import ThreeDotsWave from '../components/Tools/ThreeDotsWave';
 import Auth from '../utils/auth';
+import Guy from '../images/no-friends.svg'
 import { GET_ME } from '../utils/queries';
+import GrayButton from '../components/Tools/GrayButton';
 
 function Following() {
 
     const { data, loading } = useQuery(GET_ME);
 
     const following = data?.me?.following || {};
+    console.log(following)
 
     if (!Auth.loggedIn()){
     return <Redirect to='/welcome'/>
@@ -29,31 +32,39 @@ function Following() {
                             <ThreeDotsWave/>
                         </section>
                     ) : (
-                        following?.map((user) => (
-                              <Link to={`profile/${user._id}`} className='profile-card' key={user._id}>
-                                <section className='image-container small-image-container'>
-                                    {user.propic === '#' ? (
-                                        <p>{user.username[0].toUpperCase()}</p>
-                                    ) : (
-                                        <img className='small-pic' alt='Profile Icon' src={user.propic}/>
-                                    )}
-                                    
-                                </section>
-                                <section className='user-titles'>
-                                    <p>{user.username}</p>
-                                    {user.followers?.length === 1 ? (
-                                        <p className='light-text'>{`${user.followers?.length} Follower`}</p>
-                                    ) : (
-                                        <p className='light-text'>{`${user.followers?.length} Followers`}</p>
-                                    )}
-                                    
-                                </section>
-                                <div></div>
-                                <div></div>
-                            </Link>  
-                            
-                            
-                        ))
+                        following[0] === undefined ? (
+                            <>
+                                <img alt='Person Icon' className='person' src={Guy}/>
+                                <p className='light-text no-following'>You have no friends...</p>
+                                <Link to='/friends'><GrayButton word="Explore"/></Link>
+                            </>
+                        ) : (
+                            following?.map((user) => (
+                                <Link to={`profile/${user._id}`} className='profile-card' key={user._id}>
+                                  <section className='image-container small-image-container'>
+                                      {user.propic === '#' ? (
+                                          <p>{user.username[0].toUpperCase()}</p>
+                                      ) : (
+                                          <img className='small-pic' alt='Profile Icon' src={user.propic}/>
+                                      )}
+                                      
+                                  </section>
+                                  <section className='user-titles'>
+                                      <p>{user.username}</p>
+                                      {user.followers?.length === 1 ? (
+                                          <p className='light-text'>{`${user.followers?.length} Follower`}</p>
+                                      ) : (
+                                          <p className='light-text'>{`${user.followers?.length} Followers`}</p>
+                                      )}
+                                      
+                                  </section>
+                                  <div></div>
+                                  <div></div>
+                              </Link>  
+                              
+                              
+                          ))
+                        )
                     )}
                 </section>
             </section>
