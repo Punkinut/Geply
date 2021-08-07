7985d1a9b02778839b69f099ce7cf930d208241a
Punkinut <heypunkinut@gmail.com>
2021-08-07 11:51:06 +1000

    Added submit button and changed the CSS on the form process

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -3,13 +3,16 @@ import React, {useState} from 'react'
 import  { Link, Redirect } from 'react-router-dom'
 import Auth from '../utils/auth';
 import Arrow from '../images/left-arrow.svg'
+import Send from '../images/send.svg'
 
 function Upload() {
     const [ file, setFile ] = useState('');
     const [ caption, setCaption ] = useState('');
+    const [ hasUpload, setHasUploaded ] = useState(false);
 
     const uploadFile = (e) => {
         setFile(e.target.files[0])
+        setHasUploaded(true)
     };
 
     const uploadCaption = (e) => {
@@ -36,13 +39,19 @@ function Upload() {
                 <Link to='/profile'><img className='icon left-arrow' src={Arrow} alt='Arrow Icon'/></Link>
                     <p className='create-post'>Create Post</p>
                     <form className='upload-form' onSubmit={sumbmitUpload}>
-                        {/* Upload Form Here */}
-                        <motion.button className='small-button' whileTap={{scale: 0.9}} whileHover={{scale: 1.08}}>
-                            Upload
-                            <input className='content-upload' id='fileid' type='file' accept="image/png, image/jpeg" onChange={uploadFile} required/>
-                        </motion.button>
+                        {hasUpload ? (
+                            <motion.button className='gray-button' whileTap={{scale: 0.9}} whileHover={{scale: 1.08}}>
+                                Uploaded
+                                <input className='content-upload' id='fileid' type='file' accept="image/png, image/jpeg" onChange={uploadFile} required/>
+                            </motion.button>
+                        ) : (
+                            <motion.button className='wide-button' whileTap={{scale: 0.9}} whileHover={{scale: 1.08}}>
+                                Upload
+                                <input className='content-upload' id='fileid' type='file' accept="image/png, image/jpeg" onChange={uploadFile} required/>
+                            </motion.button>
+                        )}
                         <textarea className='caption-input light-text' placeholder='Add a caption...' maxLength='40' type='text' onChange={uploadCaption} required></textarea>
-                        <button type='submit' className='final-post'></button>
+                        <button type='submit' className='final-post'><img className='icon post-icon' alt='Send Icon' src={Send}/></button>
                     </form>
                     
                 </section>
