c3ed5923b51725fb312d967042bb5e678fb996bf
Punkinut <heypunkinut@gmail.com>
2021-08-12 15:12:24 +1000

    Updated mobile nav to have message link

--- client/src/components/Nav/MobileNav.js
+++ client/src/components/Nav/MobileNav.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Auth from '../../utils/auth';
 import Home from '../../images/Home.svg'
 import Compass from '../../images/Compass.svg'
+import Message from '../../images/Message.svg'
 import User from '../../images/User.svg'
 import { NavLink, useLocation } from 'react-router-dom'
 import { motion } from 'framer-motion';
@@ -19,8 +20,9 @@ function MobileNav() {
         exit={{ opacity: 0}}
         >
             <section className='mobile-nav'>
-                <NavLink exact activeClassName='active-icon' className={'icon'} to='/friends'><img alt='Compass Icon' src={Compass}/></NavLink>
                 <NavLink exact activeClassName='active-icon' className={'icon'} to='/'><img alt='Message Icon' src={Home}/></NavLink>
+                <NavLink exact activeClassName='active-icon' className={'icon'} to='/friends'><img alt='Compass Icon' src={Compass}/></NavLink>
+                <NavLink exact activeClassName='active-icon' className={'icon'} to='/chat'><motion.img whileHover={{scale: 1.3}} alt='Compass Icon' src={Message}/></NavLink>
                 <NavLink exact activeClassName='active-icon' className={'icon user'} to='/profile'><img alt='User Icon' src={User}/></NavLink>
             </section>
         </motion.div>
