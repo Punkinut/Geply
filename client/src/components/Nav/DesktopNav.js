d7f4dbaacf4ea98adc1a1f9352d8a065f88909b0
Punkinut <heypunkinut@gmail.com>
2021-08-12 15:08:23 +1000

    Updated desktop nav to have message link

--- client/src/components/Nav/DesktopNav.js
+++ client/src/components/Nav/DesktopNav.js
@@ -4,6 +4,7 @@ import Logo from '../../images/Logo.svg'
 import Home from '../../images/Home.svg'
 import Compass from '../../images/Compass.svg'
 import User from '../../images/User.svg'
+import Message from '../../images/Message.svg'
 import { NavLink } from 'react-router-dom'
 import Auth from '../../utils/auth';
 
@@ -18,8 +19,9 @@ function DesktopNav() {
             >
             <section className='desktop-nav'>
                     <NavLink  className={'icon active-icon desktop-icon logo'} to='/'><motion.img whileHover={{scale: 1.1}} alt='Logo Icon' src={Logo}/></NavLink>
+                    <NavLink exact activeClassName='active-icon' className={'icon desktop-icon'} to='/'><motion.img whileHover={{scale: 1.3}} alt='Message Icon' src={Home}/></NavLink>
                     <NavLink exact activeClassName='active-icon' className={'icon desktop-icon'} to='/friends'><motion.img whileHover={{scale: 1.3, rotate: '90deg'}} alt='Compass Icon' src={Compass}/></NavLink>
-                    <NavLink exact activeClassName='active-icon' className={'icon desktop-icon'} to='/'><motion.img whileHover={{scale: 1.3, borderRadius: '10px'}} alt='Message Icon' src={Home}/></NavLink>
+                    <NavLink exact activeClassName='active-icon' className={'icon desktop-icon'} to='/chat'><motion.img whileHover={{scale: 1.3}} alt='Compass Icon' src={Message}/></NavLink>
                     <NavLink exact activeClassName='active-icon' className={'icon user desktop-icon user-d'} to='/profile'><motion.img whileHover={{scale: 1.3}} alt='User Icon' src={User}/></NavLink>
             </section>
             </motion.div>
