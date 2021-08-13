72bbcb51f07867999b8aa6bb70f6589c896483ea
Punkinut <heypunkinut@gmail.com>
2021-08-13 11:33:13 +1000

    Setup basic socket server when running the app

--- client/src/pages/MainChat.js
+++ client/src/pages/MainChat.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { io } from 'socket.io-client'
 
 function MainChat() {
+    const [socket, setSocket] = useState(null);
+
+    useEffect(() => {
+        setSocket(io('ws://localhost:8900'))
+    }, [])
     return (
         <>
             <section className='page-container'>
