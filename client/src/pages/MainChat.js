f6f0470b68faf8b003d12dc01c0653cf767486d5
Punkinut <heypunkinut@gmail.com>
2021-08-13 13:31:17 +1000

    Added basic header to chat page

--- client/src/pages/MainChat.js
+++ client/src/pages/MainChat.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { useQuery } from '@apollo/client';
 import React, { useEffect, useRef } from 'react'
 import { io } from 'socket.io-client'
@@ -19,12 +20,23 @@ function MainChat() {
             console.log(users)
         })
     }, [user])
+
+
     return (
-        <>
+        <motion.div
+        initial={{ opacity: 0}}
+        animate={{ opacity: 1}}
+        exit={{ opacity: 0}}
+        >
             <section className='page-container'>
-                <p>Chat Page</p>
+                <section className='explore-container'>
+                    <p className='username account-header explore-header'>Chat</p>
+                    <div className='search-container'>
+                        <input placeholder='Search' className='search-bar'/>
+                    </div>
+                </section>
             </section>
-        </>
+        </motion.div>
     )
 }
 
