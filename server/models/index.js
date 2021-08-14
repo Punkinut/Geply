58ceda8cc91d674aa8f5a0e0909338d9370a51f6
Punkinut <heypunkinut@gmail.com>
2021-08-14 12:25:35 +1000

    Setup basic conversation and message models

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Conversation = require('./Conversation');
+const Message = require('./Message');
 
-module.exports = { User, Post };
+module.exports = { User, Post, Conversation, Message };
