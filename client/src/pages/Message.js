62a8d47a33fc189f04eaced2be5f4fb428ebcfd9
Punkinut <heypunkinut@gmail.com>
2021-08-14 12:17:09 +1000

    Added basic styling to get the ball rolling on the messages

--- client/src/pages/Message.js
+++ client/src/pages/Message.js
@@ -1,7 +1,9 @@
 import { motion } from 'framer-motion';
 import React from 'react'
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Auth from '../utils/auth';
+import Arrow from '../images/left-arrow.svg'
+import Send from '../images/send.svg'
 
 function Message() {
     if (!Auth.loggedIn()){
@@ -14,8 +16,32 @@ function Message() {
         exit={{ opacity: 0}}
         >
             <section className='page-container'>
-                <section className='message-container border'>
-
+                <section className='posts-container'>
+                    <div className='message-header'>
+                        <Link to='/chat'><img className='icon' src={Arrow} alt='Arrow Icon'/></Link>
+                        <p>Jeff Beck</p>
+                        <div></div>
+                    </div>
+                <div className='realtime-message'>
+                   <section className='comment-container'>
+                    <section className='com com2'>
+                        <div className='img-propic'>
+                            <img alt='' className='comment-pic'/>
+                        </div>
+                        <p className='real-comment light-text'>Hello my name is Tom and I was just testing the styling of this!</p>
+                    </section>
+                    <section className='com'>
+                        <p className='blue-comment blue-message light-text'>Hello my name is Tom and I was just testing the styling of this!</p>
+                    </section>
+                </section>
+                <section className='comment-nav'>
+                        <form className='comment-form'>
+                            <input className='light-text comment-input' placeholder='Type a message...' type='text' required></input>
+                            <button type='submit' className='comment-submit'><img className='icon post-icon' alt='Send Icon' src={Send}/></button>
+                        </form>
+                    </section> 
+                </div>
+                
                 </section>
             </section>
         </motion.div>
