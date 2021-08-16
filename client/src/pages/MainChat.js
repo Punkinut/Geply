a5fcb769689dc3bf7b56deb60caf43f42861dd62
Punkinut <heypunkinut@gmail.com>
2021-08-16 16:42:19 +1000

    Quick JSX fix

--- client/src/pages/MainChat.js
+++ client/src/pages/MainChat.js
@@ -50,7 +50,7 @@ function MainChat() {
                                     {/* Will have to shorten when getting data */}
                                     {messages?.filter(message => message?.conversationId === conversation._id)?.reverse()?.map((newMessage, i) => (
                                         i === 0 ? (
-                                            <p key={newMessage?._id} className='light-text'>{newMessage.text.slice(0, 23)}..</p>
+                                            <p key={newMessage?._id} className='light-text'>{newMessage.text.slice(0, 23)}</p>
                                         ) : (
                                             <div key={newMessage?._id}></div>
                                         )
